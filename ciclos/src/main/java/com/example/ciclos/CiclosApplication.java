9a0e067d4afb9761f8adf13fbd7067afcc8a40c1
AngelNavas20 <angelnavas20di@gmail.com>
2024-07-10 19:22:08 -0600

    clases

---
+++ ciclos/src/main/java/com/example/ciclos/CiclosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ciclos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiclosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiclosApplication.class, args);
+	}
+
+}
